854d3c82b7ab2d2984d3c521e8fbc94b6a743022
Kriti Prajapati <kritiprajapati19@gmail.com>
2024-01-31 20:40:14 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6">
+				<h2>Login Form</h2>
+				<form action="validation.php" method="post">
+					<div class="form-group">
+						<label>Username</label>
+						<input type="text" name="users" class="form-control">
+						
+					</div>
+				</form>
+				
+			</div>
+		</div>
+		
+	</div>
+
+</body>
+</html>
